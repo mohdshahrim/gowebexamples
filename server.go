95177911d7f17ae300f8bc70e27b4fb657d0518e
M. Shahrim <shahrim.private@gmail.com>
2023-08-17 13:11:41 +0800

    added server.go

---
+++ server.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/books/{title}/page/{page}", func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		title := vars["title"]
+		page := vars["page"]
+		fmt.Fprintf(w, "You're requested the book: %s on page %s\n", title, page)
+	})
+
+	http.ListenAndServe(":8080", r)
+}
